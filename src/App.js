d20aaa357551dedaea468c64ba0a9e1108bf4d10
Ainsley <ainsleyjyl@gmail.com>
2024-02-16 13:42:44 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-    return <div className='App'>Bakers</div>;
-}
+import React from 'react';
+
+import {
+    AboutUs,
+    Chef,
+    FindUs,
+    Footer,
+    Gallery,
+    Header,
+    Intro,
+    Laurels,
+    SpecialMenu,
+} from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+    <div>
+        <Navbar />
+        <Header />
+        <AboutUs />
+        <SpecialMenu />
+        <Chef />
+        <Intro />
+        <Laurels />
+        <Gallery />
+        <FindUs />
+        <Footer />
+    </div>
+);
 
 export default App;
