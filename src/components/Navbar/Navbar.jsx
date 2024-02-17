98eae9dcd424e1731e91802577b24c6013fe674e
Ainsley <ainsleyjyl@gmail.com>
2024-02-17 13:37:55 -0800

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+            <a href='/'>
+                <img
+                    src={images.bakers}
+                    alt='app logo'
+                />
+            </a>
+        </div>
+        <ul className='app__navbar-links'>
+            <li className='p__opensans'>
+                <a href='#about'>about</a>
+            </li>
+            <li className='p__opensans'>
+                <a href='#menu'>Menu</a>
+            </li>
+            <li className='p__opensans'>
+                <a href='#award'>Award</a>
+            </li>
+            <li className='p__opensans'>
+                <a href='#contact'>Contact</a>
+            </li>
+        </ul>
+    </nav>
 );
 
 export default Navbar;
