d91e78127e5e1aee0b1b0b938e20bb29ecff7cf3
Ainsley <ainsleyjyl@gmail.com>
2024-02-19 12:16:50 -0800

    add header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,8 +11,26 @@ const Header = () => (
         <div className='app__wrapper_info'>
             <SubHeading title='Chase the New Flavor' />
             <h1 className='app__header-h1'>The Key to Perfect Pastries</h1>
+            <p
+                className='p__opensans'
+                style={{ margin: '2rem 0' }}
+            >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi consectetur
+                laboriosam molestias odit? Eum, nulla.
+            </p>
+            <button
+                type='button'
+                className='custom__button'
+            >
+                Explore Menu
+            </button>
         </div>
-        <div className='app__wrapper_img'></div>
+        <div className='app__wrapper_img'>
+            <img
+                src={images.welcome}
+                alt='header img'
+            />
+        </div>
     </div>
 );
 
