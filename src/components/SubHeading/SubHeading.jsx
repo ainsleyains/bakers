5840518c07bf9f6bdee7cf58d7b9b85f36b50066
Ainsley <ainsleyjyl@gmail.com>
2024-02-18 21:31:47 -0800

    add header

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
+const SubHeading = ({ title }) => (
+    <div style={{ marginBottom: '1rem' }}>
+        <p className='p__cormorant'>{title}</p>
+        <img
+            src={images.mixer}
+            alt='mixer'
+            className='mixer__img'
+        />
+    </div>
 );
 
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+    <div
+        className='app__header app__wrapper section__padding'
+        id='home'
+    >
+        <div className='app__wrapper_info'>
+            <SubHeading title='Chase the New Flavor' />
+            <h1 className='app__header-h1'>The Key to Perfect Pastries</h1>
+        </div>
+        <div className='app__wrapper_img'></div>
+    </div>
 );
 
 export default Header;
