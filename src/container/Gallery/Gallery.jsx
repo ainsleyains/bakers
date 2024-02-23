26ee0db15fdf72cb5cd7ad7bb3706d0803eb0833
Ainsley <ainsleyjyl@gmail.com>
2024-02-22 20:19:37 -0800

    add gallery section

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -9,7 +9,22 @@ const Gallery = () => {
         <div className='app__gallery flex-center'>
             <div className='app__gallery-content'>
                 <SubHeading title='Instagram' />
+                <h1 className='headtext__cormorant'>Photo Gallery</h1>
+                <p
+                    className='p__opensans'
+                    style={{ color: '#AAA', marginTop: '2rem' }}
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deserunt
+                    nostrum quidem? Deserunt quisquam sunt explicabo eligendi.
+                </p>
+                <button
+                    type='button'
+                    className='custom__button'
+                >
+                    View More
+                </button>
             </div>
+            <div className='app__gallery-images'></div>
         </div>
     );
 };
